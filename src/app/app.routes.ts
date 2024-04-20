eb5f9dd80e6c5d7c841bbeb9d257d56492363de8
garima <garimasrivastava0104@gmail.com>
2024-04-21 03:29:59 +0530

    Added Login Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'financia',loadChildren:()=> import('./financia/financia.module').then(m=>m.FinanciaModule)
+    }
+];
---
+++ src/app/financia/financia.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FinanciaRoutingModule } from './financia-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FinanciaRoutingModule
+  ]
+})
+export class FinanciaModule { }
